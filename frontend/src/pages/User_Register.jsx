8ff6e553d0946a0c30c636954b766e902f03889a
Azka <azkarr11@gmail.com>
2024-04-23 12:21:32 +0700

    add register user page

---
+++ frontend/src/pages/User_Register.jsx
@@ -0,0 +1,214 @@
+import React, { useState } from "react";
+import loginImg from "../assets/login.jpg";
+import { useNavigate } from "react-router-dom";
+import { registerUser } from "../modules/fetch/user";
+
+function UserRegister() {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const navigate = useNavigate();
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    if (!e.target.username.value || !e.target.email.value || !password) {
+      alert("Semua Field Harus diisi!");
+      return;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(e.target.email.value)) {
+      setError("Invalid email address");
+      return;
+    }
+
+    try {
+      await registerUser(
+        e.target.username.value,
+        e.target.email.value,
+        password
+      );
+
+      // Set pesan sukses
+      setSuccessMessage("Registration successful!");
+
+      // Reset state password dan konfirmasi password
+      setPassword("");
+      setConfirmPassword("");
+
+      // Navigasi setelah beberapa detik
+      navigate("/login");
+    } catch (err) {
+      // Menyimpan pesan kesalahan ke state
+      setError(`Error: ${err.message}`);
+    }
+  };
+
+  return (
+    <section className="bg-gray-100 min-h-screen flex box-border justify-center items-center">
+      <div className="bg-[#dfa674] rounded-2xl flex max-w-3xl p-5 items-start">
+        <div className="md:w-1/2 px-8 py-4">
+          <h2 className="font-bold text-3xl text-[#002D74]">Register</h2>
+          <p className="text-sm mt-4 text-[#002D74]">
+            If you already a member, easily log in now.
+          </p>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit}
+          >
+            <input
+              className="p-2 mt-8 rounded-xl border"
+              type="username"
+              name="username"
+              placeholder="Username"
+            />
+            <input
+              className="p-2 rounded-xl border"
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+            <div className="relative">
+              <input
+                className="p-2 rounded-xl border w-full"
+                type={showPassword ? "text" : "password"}
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="absolute inset-y-0 right-0 px-2 py-2 text-gray-500 hover:text-gray-700 focus:outline-none"
+              >
+                {showPassword ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 absolute right-2 bottom-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 absolute right-2 bottom-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+            <div className="relative">
+              <input
+                className="p-2 rounded-xl border w-full"
+                type="password"
+                name="password"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="absolute inset-y-0 right-0 px-2 py-2 text-gray-500 hover:text-gray-700 focus:outline-none"
+              >
+                {showPassword ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 absolute right-2 bottom-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 absolute right-2 bottom-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+            {password !== confirmPassword && (
+              <span className="text-sm text-center text-red-800">
+                The password does not match
+              </span>
+            )}
+            <button
+              className="bg-[#002D74] text-white py-2 rounded-xl hover:scale-105 duration-300 hover:bg-[#206ab1] font-medium"
+              type="submit"
+              onClick={() => {
+                console.log("Sign Up clicked");
+              }} // Gantilah dengan fungsi atau tindakan yang sesuai
+            >
+              Register
+            </button>
+          </form>
+        </div>
+        <div className="md:block hidden w-1/2">
+          <img
+            className="rounded-2xl max-h-[1600px]"
+            src={loginImg}
+            alt="login form image"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default UserRegister;
